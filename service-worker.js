7b46543ae6866476d2aecffadc214ccaa97088bd
Jonas <jjangelo77@gmail.com>
2026-05-31 01:11:56 -0300

    fix: força rede no service worker para evitar cache de dados

--- service-worker.js
+++ service-worker.js
@@ -7,10 +7,8 @@ const ASSETS = [
   '/icon-512.png'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
 
 self.addEventListener('activate', e => {
